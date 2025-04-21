7a5a5839c014a9d12d96b0e8e146b16c059459e1
angon1 <kubabak@gmail.com>
2025-04-21 10:25:03 +0200

    Small updates

--- the_alchemist/workbench/base_workbench.py
+++ the_alchemist/workbench/base_workbench.py
@@ -5,6 +5,34 @@ from tools.base_tool import BaseTool
 from ingredients.base_ingredient import BaseIngredient
 
 class BaseWorkbench(ABC, BaseModel):
+    """
+    BaseWorkbench is an abstract base class that represents a workbench used for combining tools and ingredients to create a final product.
+
+    Attributes:
+        tools (List[str]): A list of tool names available in the workbench.
+        ingredients (List[str]): A list of ingredient names available in the workbench.
+
+    Methods:
+        add_tool(tool):
+            Adds a tool to the workbench.
+        
+        remove_tool(tool):
+            Removes a tool from the workbench.
+        
+        add_ingredients(ingredients: List[BaseIngredient]):
+            Adds a list of ingredients to the workbench.
+        
+        remove_ingredients(ingredients: List[BaseIngredient]):
+            Removes a list of ingredients from the workbench.
+        
+        get_final_product():
+            Returns the final product if there is exactly one ingredient in the workbench.
+            Raises a ValueError if there is more than one ingredient.
+        
+        use_tool(tool: BaseTool, ingredients: List[BaseIngredient]):
+            Uses a tool on a list of ingredients to produce a final product.
+            Raises a ValueError if any ingredient is not found in the workbench or if the tool is not found in the workbench.
+    """
     tools: List[str]
     ingredients: List[str]
 
--- the_alchemist/workbench/workbench_factory.py
+++ the_alchemist/workbench/workbench_factory.py
@@ -1,8 +1,25 @@
 from workbench.workbench import Workbench
 
 class WorkbenchFactory:
+    """
+    Factory class for creating Workbench instances.
+    """
+
     @staticmethod
     def create(data: dict):
+        """
+        Create a Workbench instance from a dictionary of data.
+
+        Args:
+            data (dict): A dictionary containing the data to initialize the Workbench.
+
+        Returns:
+            Workbench: An instance of the Workbench class initialized with the provided data.
+
+        Raises:
+            Exception: If the Workbench instance cannot be created.
+        """
+
         try:
             return Workbench(**data)
         except Exception as e:
