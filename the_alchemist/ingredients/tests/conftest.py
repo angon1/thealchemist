20eb8d3e88896c45f8ef8fb4b3a08ea5ea571d5d
angon1 <kubabak@gmail.com>
2025-05-05 18:00:28 +0200

    JB: Add docstrings

--- the_alchemist/ingredients/tests/conftest.py
+++ the_alchemist/ingredients/tests/conftest.py
@@ -7,16 +7,35 @@ from ingredients.db.mappings import registered_ingredients
 
 @pytest.fixture
 def herb_repository():
+    """Fixture for creating a HerbJsonRepository instance.
+
+    Returns:
+        HerbJsonRepository: An instance of the herb repository.
+    """
     return HerbJsonRepository()
 
 
 @pytest.fixture
 def ingredient_factory():
+    """Fixture for creating an IngredientFactory instance.
+
+    Returns:
+        IngredientFactory: An instance of the ingredient factory.
+    """
     return IngredientFactory(registered_ingredients=registered_ingredients)
 
 
 @pytest.fixture
 def ingredient_service(herb_repository, ingredient_factory):
+    """Fixture for creating an IngredientService instance.
+
+    Args:
+        herb_repository (HerbJsonRepository): The herb repository fixture.
+        ingredient_factory (IngredientFactory): The ingredient factory fixture.
+
+    Returns:
+        IngredientService: An instance of the ingredient service.
+    """
     return IngredientService(
         ingredient_repository=herb_repository, ingredient_factory=ingredient_factory
     )
